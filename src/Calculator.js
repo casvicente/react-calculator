919bb6a815905df51f0e73d3ea993f8c450a5f0c
Vicente Castelló <casvicente@gmail.com>
2020-07-21 16:15:02 -0300

    Se manejan eventos del teclado y se crea componente keypad con todos los botones

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useReducer, useEffect } from 'react';
 import './Calculator.css';
 
 
@@ -7,59 +7,96 @@ function Display (props) {
 }
 
 
-function Numpad (props) {
-    console.log("Render Numpad")
-    const numbers = [7,8,9,4,5,6,1,2,3,0];
+function Keypad ({onClick}) {
+    console.log("Render Operators");
+    
+    const handleC = () => onClick({type: 'c'});
+    const handleAC = () => onClick({type: 'ac'});
+    const handleMul = () => onClick({type: 'opp', value: 'mul'});
+    const handleDiv = () => onClick({type: 'opp', value: 'div'});
+    const handleAdd = () => onClick({type: 'opp', value: 'add'});
+    const handleSub = () => onClick({type: 'opp', value: 'sub'});
+    const handleRes = () => onClick({type: 'opp', value: 'res'});
+    const handleNum = (value) => () => onClick({type: 'num', value});
+    const numHandlers = [...Array(10).keys()].map((i) => handleNum(i));
+    
+    const handleKeypress = (e) => {
+        let keymap = {
+            'c': handleC, 
+            'a': handleAC,
+            '*': handleMul,
+            '/': handleDiv,
+            '+': handleAdd,
+            '-': handleSub,
+            'Enter':handleRes,
+        };
+        numHandlers.forEach((numHandler, number) => {
+            keymap[String(number)] = numHandler
+        });
 
-    return (<div className="Numpad">
-            {numbers.map(i => 
-                <button key={i} onClick={() => props.onNumPressed(i)}>{i}</button>
-            )}
-            <button>,</button>
-            <button></button>
-    </div>)
-}
-function Operators ({onAdd, onSub, onMul, onDiv, onC, onAC, onResult}) {
-    console.log("Render Operators")
-
-    return (<div className="Operators">
-    <button onClick={onC}>C</button><button onClick={onAC}>AC</button>
-    <button onClick={onMul}>×</button><button onClick={onDiv}>÷</button>
-    <button onClick={onAdd}>+</button><button onClick={onSub}>-</button>
-    <button onClick={onResult}>=</button><button/>
-    </div>)
-}
-
-const add = (a, b) => a + b;
-const sub = (a, b) => a - b;
-const mul = (a, b) => a * b;
-const div = (a, b) => a / b;
-const assign = (_, b) => b;
-
-
-
-function calculatorReducer(state, action) {
-    const api = {
-        add, sub, mul, div, res:assign,
+        const handler = keymap[e.key]; 
+        if (handler) {
+            handler(); 
+            e.preventDefault();
+        }
     };
 
-    const intialState = {
-        acc: 0,
-        operation: api.assign,
-        current: null,
-    }
+    useEffect(() => {
+        document.addEventListener("keydown", handleKeypress);
+        return (()=> document.removeEventListener("keydown", handleKeypress));
+    })
+
+    return (<div className="Keypad">
+                <button onClick={numHandlers[7]}>7</button>
+                <button onClick={numHandlers[8]}>8</button>
+                <button onClick={numHandlers[9]}>9</button>
+                <button onClick={handleC}>C</button>
+                <button onClick={handleAC}>AC</button>
+                <button onClick={numHandlers[4]}>4</button>
+                <button onClick={numHandlers[5]}>5</button>
+                <button onClick={numHandlers[6]}>6</button>
+                <button onClick={handleMul}>×</button>
+                <button onClick={handleDiv}>÷</button>
+                <button onClick={numHandlers[1]}>1</button>
+                <button onClick={numHandlers[2]}>2</button>
+                <button onClick={numHandlers[3]}>3</button>
+                <button onClick={handleAdd}>+</button>
+                <button onClick={handleSub}>-</button>
+                <button onClick={numHandlers[0]}>0</button>
+                <button/>
+                <button/>
+                <button onClick={handleRes}>=</button>
+            </div>)
+}
+
+
+
+const api = {
+    add : (a, b) => a + b,
+    sub : (a, b) => a - b,
+    mul : (a, b) => a * b,
+    div : (a, b) => a / b,
+    res : (_, b) => b,
+};
+
+const intialState = {
+    acc: 0,
+    operation: api.res,
+    current: null,
+};
+
+function calculatorReducer(state, action) {
 
     const accumulate = (s)  => s.current == null ? s:{
         acc: s.operation(s.acc, s.current),
-        operator: assign,
+        operator: api.res,
         current: null,
     };
-
     switch (action.type) {
         case 'num':
             return {...state, current:  state.current == null ? action.value : state.current*10 + action.value};
         case 'opp':
-            return {...accumulate(state), operation:api[action.value]};
+            return {...accumulate(state), operation: api[action.value]};
         case 'ac':
             return intialState;
         case 'c':
@@ -67,60 +104,13 @@ function calculatorReducer(state, action) {
         default:
             return intialState;
     }
-
 }
 
 function Calculator() {
-    const [acc, setAcc] = useState(0);
-    const [operation, setOperation] = useState(() => assign);
-    const [current, setCurrent] = useState(null);
-
-    const accumulate = ()  => {
-        if (current != null) {
-            const newAcc = operation(acc, current);
-            setAcc(newAcc);
-            setCurrent(null) 
-        }
-    }
-    const handleNumPressed = (digit) => {
-        console.log("Num pressed " + digit);
-        setCurrent( current == null ? digit : current*10 + digit);
-    }  
-
-    const handleOperator = (operator) => () => {
-        console.log("Add pressed") 
-        accumulate();
-        setOperation(() => operator);
-    }
-
-    const handleAC = () => {
-        setAcc(0);
-        setOperation(() => assign);
-        setCurrent(null);
-    }
-
-    const handleC = () => {
-        if(current!=null){
-            setCurrent(null);
-        } else  {
-            handleAC();
-        }
-    }
-
+    const [{current, acc}, dispatch] = useReducer(calculatorReducer, intialState);
     return (<div className="Calculator">
                 <Display value={current ?? acc} />
-                <div className="Keypad">
-                    <Numpad onNumPressed = {handleNumPressed}/>
-                    <Operators 
-                        onAdd = {handleOperator(add)} 
-                        onSub = {handleOperator(sub)}
-                        onMul = {handleOperator(mul)}
-                        onDiv = {handleOperator(div)}
-                        onC = {handleC}
-                        onAC = {handleAC}
-                        onResult = {handleOperator(assign)}
-                    />
-                </div>
+                <Keypad onClick = {dispatch}/>
             </div>);
 }
 
