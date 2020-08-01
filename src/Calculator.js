63f00291afddd92925458b045655449559d43487
casvicente <casvicente@gmail.com>
2020-07-31 22:18:39 -0300

    Refactor

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,11 +1,7 @@
 import React, { useReducer, useEffect } from 'react';
 import './Calculator.css';
-
 import Keypad from './Keypad';
-
-function Display (props) {
-    return <div className="Display">{props.value}</div>
-}
+import Display from './Display';
 
 const api = {
     add : (a, b) => a + b,
@@ -15,35 +11,41 @@ const api = {
     res : (_, b) => b,
 };
 
-const intialState = {
+const digits = [...Array(10).keys()]; 
+const operations = Object.keys(api);
+
+const initialState = {
     acc: 0,
     operation: api.res,
     current: null,
 };
 
+const accumulate = (s) => s.current == null ? s:{
+    acc: s.operation(s.acc, s.current),
+    operation: api.res,
+    current: null,
+};
+
+const handleOperation = (op) => (state) => ({
+        ...accumulate(state),
+        operation: api[op]
+});
+
+const handleDigit = (digit) => (state) => ({
+        ...state,
+        current: state.current == null ? digit : state.current * 10 + digit
+});
+
+//Handlers para transiciones de estados en el reducer
+let actionHandlers = {
+    'key:AC' : (state) => initialState,
+    'key:C' : (state) => state.current != null? {...state, current: null}: initialState,
+    ...Object.fromEntries(operations.map(op => [`key:${op}`, handleOperation(op)])),
+    ...Object.fromEntries(digits.map(d => [`digit:${d}`, handleDigit(d)])),
+}
+
 function calculatorReducer(state, action) {
-    const accumulate = (s)  => s.current == null ? s:{
-        acc: s.operation(s.acc, s.current),
-        operator: api.res,
-        current: null,
-    };
-    const [actionType, actionValue] = action.split(':');
-    switch (actionType) {
-        case 'digit':
-            const digit = Number(actionValue);
-            return {...state, current:  state.current == null ? digit : state.current*10 + digit};
-        case 'key':
-            switch (actionValue) {
-                case 'AC':
-                    return intialState;
-                case 'C':
-                    return state.current != null? {...state, current: null}: intialState;
-                default:
-                    return api[actionValue]?{...accumulate(state), operation: api[actionValue]}:intialState;
-            }
-        default:
-            return intialState;
-    }
+    return actionHandlers[action]? actionHandlers[action](state): initialState;    
 }
 
 const handleKeypress = (dispatch) => (e) => {
@@ -57,7 +59,7 @@ const handleKeypress = (dispatch) => (e) => {
         'Enter':'key:res',
     };
 
-    [...Array(10).keys()].forEach(i => {
+    digits.forEach(i => {
         keymap[i] = 'digit:' + i;
     });
 
@@ -69,8 +71,8 @@ const handleKeypress = (dispatch) => (e) => {
 };
 
 function Calculator() {
-    const [{current, acc}, dispatch] = useReducer(calculatorReducer, intialState);
-    useEffect( () => {
+    const [{current, acc}, dispatch] = useReducer(calculatorReducer, initialState);
+    useEffect(() => {
         const listener = handleKeypress(dispatch);
         document.addEventListener("keypress", listener);
         return () => document.removeEventListener("keypress", listener);
---
+++ src/Display.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Display.css';
+
+function Display ({value}) {
+    return <div className="Display">{value}</div>
+}
+
+export default Display;
